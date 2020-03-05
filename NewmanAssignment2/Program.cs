43aeed24afaae5259fb72cfb3845d46326b70c56
David Newman <david.newman@wvumedicine.org>
2020-03-05 18:45:30 -0500

    Add project files.

---
+++ NewmanAssignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewmanAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
